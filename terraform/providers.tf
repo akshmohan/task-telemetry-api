0d162c5140a7619f6adab43c19da91c09d89ec55
akshmo <akshaymohan.career@gmail.com>
2026-01-25 09:27:08 +0530

    Fix ECR paths and remove PVC for EKS deployment

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,28 +2,10 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.35.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.17.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
   }
 }
 
