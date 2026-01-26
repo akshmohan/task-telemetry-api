cecc41901e81e4b2e5bed387c7e9165bdc557f92
akshmo <akshaymohan.career@gmail.com>
2026-01-26 10:27:48 +0530

    Fix flask.yaml

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,9 +11,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     nodes = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
 
       instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
