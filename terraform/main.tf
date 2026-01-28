60a28eba65d88f025cee1307255747514ac5bad1
akshmo <akshaymohan.career@gmail.com>
2026-01-28 15:03:39 +0530

    Final commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# 1. S3 Bucket - Storage for your App
 resource "aws_s3_bucket" "task_storage" {
   bucket = "task-telemetry-storage-akshay-740994"
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,13 +5,12 @@ module "vpc" {
   name = "task-telemetry-vpc"
   cidr = "10.0.0.0/16"
 
-  # We need at least 2 zones for EKS high availability
   azs             = ["ap-south-1a", "ap-south-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true # EKS nodes need this to talk to the internet
-  single_nat_gateway = true # Saves money (only creates one instead of two)
+  enable_nat_gateway = true 
+  single_nat_gateway = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = "1"
